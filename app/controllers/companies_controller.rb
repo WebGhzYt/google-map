32bc4930b4180775cdb6fc2f9b88b1f7389548dc
rhtprwl <rohit.porwal@ongraph.com>
2015-03-13 13:46:52 +0530

    2nd Commit for new and edit action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,12 +10,44 @@ class CompaniesController < ApplicationController
   end
   def show
   	   @company = Company.find(params[:id])
-
   end
 
   def new
+    @company = Company.new
+  end
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
   end
 
+
   def edit
+    @company = Company.find(params[:id])
   end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update_attributes(company_params)
+        # if update is successful
+    else 
+        # if update is unsuccessful
+    end
+    redirect_to @company
 end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :lat, :lng)
+  end
+
+
+end
+
+
